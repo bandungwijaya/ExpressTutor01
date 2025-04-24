e14e8791672ee36f0ae7bb8d263abfbbc977bfe0
bandungwijaya <bandung.wijaya@gmail.com>
2025-04-24 09:41:21 +0700

    insert database

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//import body parser
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
 //import route tags
 const tagsRouter = require('./routes/tags');
 app.use('/api/tags', tagsRouter); // use route tags di Express
